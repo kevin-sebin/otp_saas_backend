8a8a4c2d710df6bdbf24407e1b7a0dd5894e78dd
Kevin Sebin <kevseb2005@gmail.com>
2026-05-15 12:09:15 +0530

    verify file update

--- verify.py
+++ verify.py
@@ -1,17 +1,17 @@
 import requests
 
-API_KEY = "ob2qXDyVZBIs_-_tdk3f21yyMNa9dzXZ7GTfHlgPyMo"
+API_KEY = "YOUR_API_KEY"
 
 url = "http://127.0.0.1:8000/send-otp"
-payload = {"email": "kevinsebinkk@gmail.com"}
+payload = {"email": "registered_email.com"}
 headers = {"x-api-key": API_KEY}
 response = requests.post(url, json=payload, headers=headers)
 print(response.status_code)
 print(response.text)
 
 url = "http://127.0.0.1:8000/verify"
-headers = {"x-api-key": "ob2qXDyVZBIs_-_tdk3f21yyMNa9dzXZ7GTfHlgPyMo"}
-data = {"email": "kevinsebinkk@gmail.com", "otp": str(input())}
+headers = {"x-api-key": API_KEY}
+data = {"email": "registered_email.com", "otp": str(input())}
 response = requests.post(url, json=data,headers=headers)
 print(response.status_code)
 print(response.text)
